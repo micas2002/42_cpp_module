f42ae93eb2c23148180a022ae31ff31766776471
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-08-11 20:31:07 +0100

    added more tests and some new lines for better readability

--- cpp_04/ex02/sources/main.cpp
+++ cpp_04/ex02/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 17:30:31 by mibernar          #+#    #+#             */
-/*   Updated: 2023/07/08 13:18:04 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/08/11 20:24:55 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 int	main(void)
 {
+	
 	// const Animal k;//class cant be instantiable.
 	// const Animal* l = new Animal();//class cant be instantiable.
 	const Animal* j = new Dog();
@@ -23,37 +24,47 @@ int	main(void)
 	
     delete j;//should not create a leak
     delete i;
-
-	Dog*	dog = new Dog();
-	Dog*	dog2 = new Dog();
-
-	dog->getBrain()->ideas[0] = "hello";
-	dog->getBrain()->ideas[1] = "world";
-
-	std::cout << "dog ideas:" << std::endl;
-	for (int i = 0; i < 2; i++) std::cout << "Idea: " << dog->getBrain()->ideas[i] << std::endl;
+	
 	std::cout << std::endl;
-	std::cout << "dog2 ideas" << std::endl;
-	for (int i = 0; i < 2; i++) std::cout << "Idea: " << dog2->getBrain()->ideas[i] << std::endl;
+	std::cout << std::endl;
+	
+    Dog basic;
+    {
+        Dog tmp = basic;
+    }
+	
+	std::cout << std::endl;
 	std::cout << std::endl;
 
-	std::cout << "dog2 = dog" << std::endl;
-	*dog2 = *dog;
+    const Animal* a = new Dog();
+    const Animal* b = new Cat();
+    std::cout << a->getType() << " " << std::endl;
+    std::cout << b->getType() << " " << std::endl;
+    a->makeSound();
+    b->makeSound();
+
+    delete a;
+    delete b;
+
 	std::cout << std::endl;
-	std::cout << "dog ideas:" << std::endl;
-	for (int i = 0; i < 2; i++) std::cout << "Idea: " << dog->getBrain()->ideas[i] << std::endl;
-	std::cout << std::endl;
-	std::cout << "dog2 ideas" << std::endl;
-	for (int i = 0; i < 2; i++) std::cout << "Idea: " << dog2->getBrain()->ideas[i] << std::endl;
 	std::cout << std::endl;
 
-	std::cout << "dog idea 2 is cat" << std::endl;
-	dog->getBrain()->ideas[1] = "cat";
-	for (int i = 0; i < 2; i++) std::cout << "Idea: " << dog->getBrain()->ideas[i] << std::endl;
+    Cat c;
+    Cat d;
+    c = d;
+    std::cout << c.getType() << std::endl;
+		
 	std::cout << std::endl;
-	std::cout << "dog2 ideas" << std::endl;
-	for (int i = 0; i < 2; i++) std::cout << "Idea: " << dog2->getBrain()->ideas[i] << std::endl;
 	std::cout << std::endl;
 
-    return (0);
+	int	x = 0;
+    const Animal	*animals[4] = {new Dog(), new Dog(), new Cat(), new Cat()};
+
+    while (x < 4)
+	{
+        delete animals[x];
+		x++;
+    }
+
+	return (0);
 }
