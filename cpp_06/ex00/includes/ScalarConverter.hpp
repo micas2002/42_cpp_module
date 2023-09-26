1eab09374356d94457191d16acada5214eaaf488
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-09-26 16:47:38 +0100

    starting ex00

---
+++ cpp_06/ex00/includes/ScalarConverter.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 16:08:27 by mibernar          #+#    #+#             */
+/*   Updated: 2023/09/26 16:41:05 by mibernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALAR_CONVERTER_HPP
+# define SCALAR_CONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter
+{
+	private:
+		void std::string	input;
+		void char			c;
+		void int			i;
+		void float			f;
+		void double			d;
+
+	public:
+		
+};
+
+#endif
