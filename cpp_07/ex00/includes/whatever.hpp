b7dbc6da8aaa1517ee6203e8ff724abb78579871
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-09-29 17:20:55 +0100

    starting ex00

---
+++ cpp_07/ex00/includes/whatever.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/29 17:11:30 by mibernar          #+#    #+#             */
+/*   Updated: 2023/09/29 17:19:33 by mibernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+template <typename T> void swap(T x, T y)
+{
+	T temp;
+	
+	temp = &x;
+	&x = &y;
+	&y = &temp;
+}
+
+template <typename T> T min(T x, T y)
+{
+	if (x == y)
+		return (y);
+	return (x < y) ? x : y;
+}
+
+template <typename T> T max(T x, T y)
+{
+	if (x == y)
+		return (y);
+	return (x > y) ? x : y;
+}
